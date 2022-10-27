f5e1611ab5a9565cf08e64752b398be9990f90af
Sugut <sugutvictorine@gmail.com>
2022-10-27 18:55:42 +0300

    commit tree ee7e95533891c17b28af83f9e9665378a73aecf6

---
+++ lib/insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO bears (name,age,sex,color,temperament,alive) 
+VALUES
+       ('Mr. Chocolate','67','M','blue','sanguine','1'),
+       ('Rowdy','32','M','brown','sanguine','1'),
+       ('Tabitha', '44','F','pink','sanguine','1'),
+       ('Sergeant Brown','34','M','black','sanguine','0'),
+       ('Melissa','52','F','orange','sanguine','0'),
+       ('Grinch','89','M','yellow','sanguine','1'),
+       ('Wendy','21','F','red','sanguine','0');
+INSERT INTO bears (age, sex,color,temperament,alive)
+VALUES ('7' , 'F', 'black','choleric', '1');
